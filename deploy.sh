3724ac8a5a22759f19225d29755a56d9f76e6159
Ivey Topaz <iveytopaz@gmail.com>
2017-11-27 19:55:28 +0000

    Tag deployments with time for differnetiation

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 # bin/bash
 eval $(minikube docker-env)
-docker build -t data-service:v1 .
-kubectl set image deployment/data-service data-service=data-service:v1
+sha=$(git log -n 1 --pretty=format:"%H")
+tag=${sha:0:6} 
+time=$(date +"%H-%M-%S") # So we can deploy with out new commits
+docker build -t data-service:sha-$tag-time-$time .
+kubectl set image deployment/data-service data-service=data-service:sha-$tag-time-$time
+sleep 5
 minikube service data-service
