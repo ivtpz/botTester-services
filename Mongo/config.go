7484f69757c6457b695859c7a7cc435b970f33fd
Ivey Topaz <iveytopaz@gmail.com>
2017-12-24 11:10:45 +0000

    Add indexes and currencies

--- Mongo/config.go
+++ Mongo/config.go
@@ -1,14 +1,16 @@
 package Mongo
 
 import (
-  "fmt"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
 type DataStore struct {
-	Session *mgo.Session
+	Session    *mgo.Session
+	Currencies []string
 }
 
 type Candle struct {
@@ -35,48 +37,71 @@ func (cndle *Candle) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-func (ds *DataStore) FindCandles(tableName string, start int, end int) ([]Candle) {
-  s := ds.Session.Copy()
-  defer s.Close()
-  c := s.DB("gdax_market_history").C(tableName)
-  var result []Candle
-  err := c.Find(bson.M{"time": bson.M{"$gt": start, "$lt": end, }}).All(&result)
-  if err != nil {
-    panic(err)
-  }
-  return result
+func (ds *DataStore) FindCandles(dbName string, tableName string, start int, end int) []Candle {
+	s := ds.Session.Copy()
+	defer s.Close()
+	c := s.DB(dbName).C(tableName)
+	var result []Candle
+	err := c.Find(bson.M{"time": bson.M{"$gt": start, "$lt": end}}).All(&result)
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
 
-func (ds *DataStore) AddCandle(tableName string, candle Candle) {
-  s := ds.Session.Copy()
-  defer s.Close()
-  c := s.DB("gdax_market_history").C(tableName)
-  err := c.Insert(candle)
-  if err != nil && !mgo.IsDup(err) {
-    panic(err)
-  }
+func (ds *DataStore) AddCandle(dbName, tableName string, candle Candle) {
+	s := ds.Session.Copy()
+	defer s.Close()
+	c := s.DB(dbName).C(tableName)
+	err := c.Insert(candle)
+	if err != nil && !mgo.IsDup(err) {
+		panic(err)
+	}
+}
+
+func (ds *DataStore) AddCurrency(currency string) {
+	ds.Currencies = append(ds.Currencies, currency)
+}
+
+func (ds *DataStore) GetCurrencies() []string {
+	return ds.Currencies
+}
+
+func setIndexes(session *mgo.Session, db string, collection string) {
+	fmt.Printf("Checking indexes for collection %s", collection)
+	c := session.DB(db).C(collection)
+
+	index := mgo.Index{
+		Key:        []string{"time"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+
+	err := c.EnsureIndex(index)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func (ds *DataStore) EnsureIndex() {
 	session := ds.Session.Copy()
 	defer session.Close()
-  
-  markets := [2]string{"ETH-USD-10", "ETH-USD-300"}
-  for _, market := range markets {
-		fmt.Printf("Checking indexes for collection %s", market)
-    c := session.DB("gdax_market_history").C(market)
 
-    index := mgo.Index{
-      Key:        []string{"time"},
-      Unique:     true,
-      DropDups:   true,
-      Background: true,
-      Sparse:     true,
-    }
+	gdaxMarkets := [2]string{"ETH-USD-10", "ETH-USD-300"}
+	for _, market := range gdaxMarkets {
+		setIndexes(session, "gdax_market_history", market)
+	}
 
-    err := c.EnsureIndex(index)
-    if err != nil {
-      panic(err)
-    }
-  }
+	poloMarkets := [1]string{"ETH-USD-300"}
+	for _, market := range poloMarkets {
+		setIndexes(session, "poloniex_market_history", market)
+	}
+
+	for _, curr := range ds.Currencies {
+		// Currencies tracked with websocket - all stored as 10s data
+		table := curr + "-USD-10"
+		setIndexes(session, "poloniex_market_history", table)
+	}
 }
